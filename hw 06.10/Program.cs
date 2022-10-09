e948ea14af03073fbef49fcd3e9111882027e8de
1 <1@DESKTOP-SUUQLOE>
2022-10-09 19:40:53 +0300

    Добавьте файлы проекта.

---
+++ hw 06.10/Program.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw_06._10
+{
+
+    struct Ded
+    {
+        public string name;
+        public byte vorch;
+        public string[] phrases;
+        public byte blows;
+
+
+        public Ded(string Name, byte Vorch, string[] Phrases, byte Hit)
+        {
+            this.name = Name;
+            this.vorch = Vorch;
+            this.phrases = Phrases;
+            this.hit = Hit;
+        }
+    }
+
+    class Program
+    {
+
+
+        public static void task_1_ReshYravn(double a, double b, double c)
+        {
+            double D = (a * a + 4 * a * c);
+            if (D > 0)
+            {
+                double x1 = (-b + Math.Sqrt(D) / 2 * a);
+                double x2 = (-b - Math.Sqrt(D) / 2 * a);
+                Console.WriteLine($"Первый корень равен {x1}, второй корень равен {x2}");
+            }
+            else if (D == 0)
+            {
+                double x1 = (-b + Math.Sqrt(D) / 2 * a);
+                Console.WriteLine($"Единственный корень равен {x1}");
+
+            }
+            else
+            {
+                Console.WriteLine("Корней нет");
+            }
+
+        }
+        public static void Task_2()
+        {
+            int[] mas = new int[20];
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next(0, 100);
+                Console.Write(mas[i] + " ");
+
+            }
+
+        }
+       
+        public static void task_3()
+        {
+            int[] num;
+            int temp;
+            for (int m = 0; m < num.Length; m++)
+            {
+                for (int j = 0; j < num.Length - 1 - m; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        temp = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = temp;
+                    }
+                }
+            }
+            for (int m = 0; m < num.Length; m++)
+            {
+                Console.WriteLine(num[m]);
+            }
+        }
+
+        public static void task_4_Sortirovka(ref int umn, out double srar, ref int sum, params int[] array1)
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum = sum + array1[i];
+            }
+            for (int i = 0; i < array1.Length; i++)
+            {
+                umn = umn * array1[i];
+            }
+            srar = sum / array1.Length;
+
+        }
+        public static void task_5()
+        {
+            bool run = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите число в диапозоне 0-9");
+                    int chislo = int.Parse(Console.ReadLine());
+                    string[] PictureNumber = new string[10];
+                    PictureNumber[0] = "###" + "\n" + "# #" + "\n" + "# #" + "\n" + "# #" + "\n" + "###";
+                    PictureNumber[1] = " # " + "\n" + "## " + "\n" + " # " + "\n" + " # " + "\n" + "###";
+                    PictureNumber[2] = " # " + "\n" + "# #" + "\n" + "  #" + "\n" + " # " + "\n" + "###";
+                    PictureNumber[3] = "## " + "\n" + "  #" + "\n" + " # " + "\n" + "  #" + "\n" + "## ";
+                    PictureNumber[4] = "# #" + "\n" + "# #" + "\n" + "###" + "\n" + "  #" + "\n" + "  #";
+                    PictureNumber[5] = "###" + "\n" + "#  " + "\n" + "###" + "\n" + "  #" + "\n" + "### ";
+                    PictureNumber[6] = "###" + "\n" + "#  " + "\n" + "###" + "\n" + "# #" + "\n" + "###";
+                    PictureNumber[7] = "####" + "\n" + "  #" + "\n" + " ###" + "\n" + " # " + "\n" + " # ";
+                    PictureNumber[8] = "###" + "\n" + "# #" + "\n" + " # " + "\n" + "# #" + "\n" + "###";
+                    PictureNumber[9] = "###" + "\n" + "# #" + "\n" + "###" + "\n" + "  #" + "\n" + "###";
+
+                    if (chislo >= 0 && chislo <= 9)
+                    {
+                        Console.WriteLine(PictureNumber[chislo]);
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Thread.Sleep(3000);
+                        Console.ResetColor();
+                        Console.Clear();
+                        Console.WriteLine("Ошибка");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введенное вами число некорректно ");
+                }
+                Console.WriteLine("Хотите продолжижть?(если нет - exit/закрыть)");
+                string ex = Console.ReadLine();
+                if (ex == "exit" || ex == "закрыть")
+                {
+                    run = false;
+                }
+            } while (run == true);
+
+        }
+
+        public static int ded(string[] words, params string[] mats)
+        {
+            int fingal = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < mats.Length; j++)
+                {
+                    if (mats[j] == words[i])
+                    {
+                        fingal++;
+                    }
+                }
+            }
+            return fingal;
+        }
+        int[] numb = { 1, 2, 4, 6, 8, 10, 12, -34 };
+        public static int FindPivot(int[] numb, int minIndex, int maxIndex)
+        {
+
+            int pivot = minIndex - 1;
+            int temp = 0;
+            for (int i = minIndex; i < maxIndex; i++)
+            {
+                if (numb[i] < numb[maxIndex])
+                {
+                    pivot++;
+                    temp = numb[pivot];
+                    numb[i] = temp;
+                }
+            }
+            pivot++;
+            temp = numb[pivot];
+            numb[pivot] = numb[maxIndex];
+            numb[maxIndex] = temp;
+            return pivot;
+
+        }
+        static int[] QuickSort(int[] numb, int maxIndex, int minIndex)
+        {
+            if (minIndex >= maxIndex)//условие при котором мин индекс больше или равен макс,тогда масс будет сост из одного элемента 
+            {
+                return numb;
+            }
+            int pivot = FindPivot(numb, minIndex, maxIndex);
+            QuickSort(numb, minIndex, pivot - 1);
+            QuickSort(numb, pivot + 1, maxIndex);
+            return numb;// чтобы вызвать,в основом методе пишем 
+            // int[] numb={chisla};
+            //numb = QuicSort(numb,0,numb.Length-1);
+            //for(int i = 0; i <numb.Length;i++
+            //{Console.WriteLine(numb[i]);}}
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Введите коэффиценты a,b,c для решения квадратного уравнения");
+            double koef1 = double.Parse(Console.ReadLine());
+            double koef2 = double.Parse(Console.ReadLine());
+            double koef3 = double.Parse(Console.ReadLine());
+            task_1_ReshYravn(koef1, koef2, koef3);
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 2");
+            Console.Write("Введите первое число ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] == n2)
+                {
+                    mas[i] = n1;
+                    continue;
+                }
+                if (mas[i] == n1)
+                {
+                    mas[i] = n2;
+                    continue;
+                }
+            }
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i]);
+
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("Массив:");
+            int[] num = { 1, 8, 7, -9, 4, 2, 15, 98 };
+            for (int m = 0; m < num.Length; m++)
+            {
+                Console.WriteLine(num[m]);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 4");
+            Console.Write("Массив - ");
+            int[] array1 = { 1, 2, 3, 4 };
+            for (int i = 0; i < array1.Length; i++)
+                Console.Write(array1[i] + " ");
+            int sum = 0;
+            int umn = 1;
+            double srar;
+            task_4_Sortirovka(ref umn, out sr, ref sum, array1);
+            Console.WriteLine();
+            Console.WriteLine($"Сумма = {sum}, Произведение = {umn}, Среднее арифметическое = {srar}");
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 5");
+            task_5();
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 6");
+            string[] mats1 = { "сволочи!", "твари", "подонки" };
+            Ded ded1 = new Ded("Зиният Зиниятович", 2, mats1, 0);
+            string[] mats2 = { "засранцы", "тваритвари", "простофили!", "кровопийцы" };
+            Ded ded2 = new Ded("Акакий Акакиевич", 4, mats2, 0);
+            string[] mats3 = { "бестолочи!", "шмары!", "гады", "алкоголики", "душегубы", "дуралеи" };
+            Ded ded3 = new Ded("Витька Бар", 5, mats3, 0);
+            string[] mats4 = { "бабник", "мудаки", "ебоба", "лицемеры" };
+            Ded ded4 = new Ded("Злюк Злюкович", 6, mats4, 0);
+            string[] mats5 = { "вруны", "прохиндяи", "плохиши" };
+            Ded ded5 = new Ded("Ворчун Ворчунович", 1, mats5, 0);
+            string[] slova = { "что нынче молодежь творит", "ты погляди на них", "твари", "бестолочи", "гады", "вот прохиндяи" };
+            Console.WriteLine("Количество финаглов первого деда от бабки: " + ded(slova, mats1));
+            Console.WriteLine("Количество  финаглов второго деда от бабки: " + ded(slova, mats2));
+            Console.WriteLine("Количество  финаглов третьего деда от бабки: " + ded(slova, mats3));
+            Console.WriteLine("Количество  финаглов четвертого деда от бабки: " + ded(slova, mats4));
+            Console.WriteLine("Количество  финаглов пятого деда от бабки: " + ded(slova, mats5));
+            Console.ReadKey();
+
+            Console.WriteLine("Задание 7");
+            int[] numb = { 1, 2, 4, 6, 8, 10, 12, -34 };
+            numb = QuickSort(numb, 0, numb.Length - 1);
+            for (int i = 0; i < numb.Length; i++)
+            {
+                Console.WriteLine(numb[i]);
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } }
+
+
+
+
+
+
+
+
---
+++ tumakov5gl/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tumakov5gl
+{
+    internal class Program
+    {
+        public static double MaxChislo(double x, double y)
+        {
+            return Math.Max(x, y);
+        }
+        public static void PermutationRef(ref double num1, ref double num2)
+        {
+            double temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        public static bool FactorialRef(ref int num)
+        {
+            int nomer = num;
+            num = 1;
+            for (int i = 1; i <= nomer; i++)
+                try
+                {
+                    checked
+                    {
+                        num *= i;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        static int Factorial_recurs(int f)
+        {
+            int rez = 1;
+            do
+            {
+                if (f < 0)
+                {
+                    Console.WriteLine("Введенное вами число некорректно");
+                }
+                else if (f == 0)
+                {
+                    rez = 1;
+                }
+                else
+                {
+                    rez = f * rez;
+                    f = f - 1;
+                }
+
+            } while (f > 0);
+            Console.WriteLine($"Факториал введенного вами числа равен {rez}");
+            return rez;
+        }
+
+        public static int NOD(int val1, int val2)
+        {
+            while ((val1 != 0) && (val2 != 0))
+            {
+                if (val1 > val2)
+                {
+                    val1 = val1- val2;
+                }
+                else
+                    val2 = val2-val1;
+            }
+
+            return Math.Max(val1, val2);
+        }
+
+        public static int NOD_(int a, int b, int c)
+        {
+            int Nod = Math.Min(a, Math.Min(a, b));
+            for (; Nod > 1; Nod--)
+            {
+                if (a % Nod == 0 && b % Nod == 0 && c % Nod == 0)
+                    break;
+            }
+            return Nod;
+        }
+
+        static int Fibonachi_recurs(int num)
+        {
+            int a1 = 0;
+            int a2 = 1;
+            if (num <= 1)
+                    
+                return 0;
+            for (int i = 2; i < num; i++)
+            {
+                int temp = a1;
+                a1 = a2;
+                a2 = a2 + temp;
+            }
+            return a2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" задание 5.1");
+            Console.Write($"Введите первое число: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write($"Введите второе число : ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Максимальное из этих чисел: {MaxChislo(a, b)}");
+            Console.WriteLine();
+
+
+            Console.WriteLine("задание 5.2");
+            Console.WriteLine("Введите 2 числа: ");
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Числа до перестановки: {num1}, {num2}");
+            Console.WriteLine($"Числа после перестановки: {num2}, {num1}");
+            Console.WriteLine();
+
+
+            Console.WriteLine(" задание 5.3");
+            Console.Write("Введите число, факториал которого нужно вычислить: ");
+            int chislo = int.Parse(Console.ReadLine());
+            bool result = FactorialRef(ref chislo);
+            Console.WriteLine($"{Convert.ToString(result)}, Факториал введенного вами числа равен {chislo}");
+            Console.WriteLine();
+
+
+            Console.WriteLine(" задание 5.4");
+            Console.Write("Введите число, факториал которого нужно вычислить: ");
+            int chisloo = int.Parse(Console.ReadLine());
+            Factorial_recurs(chisloo);
+
+
+            Console.WriteLine("задание 5.1");
+            Console.WriteLine("Введите 2 числа для вычисления НОД ");
+            int a1 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            int Nod1 = NOD(a1, b1);
+            Console.WriteLine($"Наибольший общий делитель равен {Nod1}");
+
+            Console.WriteLine("Введите 3 числа для вычисления НОД ");
+            int a2 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            int c2 = int.Parse(Console.ReadLine());
+            int Nod2 = NOD_(a2, b2, c2);
+            Console.WriteLine($"Наибольший общий делитель равен {Nod2}");
+            Console.WriteLine();
+
+            Console.WriteLine(" задание 5.2");
+            Console.Write("Введите номер элемента ряла Фибоначчи: ");
+            int nomer = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Элемент под номером {nomer} - {Fibonachi_recurs(nomer)}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
